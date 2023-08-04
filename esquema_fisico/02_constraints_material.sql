d52c25ae578b26b1226963b578fa73a93017910a
pinguim11 <52601915+pinguim11@users.noreply.github.com>
2023-08-04 20:11:22 -0300

    Update 02_constraints_material.sql
    ajuste dos nomes das variáveis material_anterior para material_anterior_id e codigo_componente para componente_curricular_codigo

--- esquema_fisico/02_constraints_material.sql
+++ esquema_fisico/02_constraints_material.sql
@@ -1,2 +1,2 @@
-ALTER TABLE material ADD CONSTRAINT FK_codigo_componente FOREIGN KEY (codigo_componente) REFERENCES componente_curricular(codigo);
-ALTER TABLE material ADD CONSTRAINT FK_material_anterior FOREIGN KEY (material_anterior) REFERENCES material(material_id);
+ALTER TABLE material ADD CONSTRAINT FK_componente_curricular FOREIGN KEY (componente_curricular_codigo) REFERENCES componente_curricular(codigo);
+ALTER TABLE material ADD CONSTRAINT FK_material_anterior FOREIGN KEY (material_anterior_id) REFERENCES material(material_id);
